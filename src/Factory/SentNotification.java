3bd2ac2b94738e6d4f32f20b904a0e013a0b9491
Esimgali <esimgalikhamitov2005@gmail.com>
2023-11-03 21:20:21 +0600

    123

--- src/Factory/SentNotification.java
+++ src/Factory/SentNotification.java
@@ -1,6 +1,5 @@
 package Factory;
 
 public abstract class SentNotification {
-    public void sentMassage(String username){
-    }
+    public void sentMassage(String username){}
 }
--- src/Subscribe/CreatNewCar.java
+++
@@ -1,4 +0,0 @@
-package Subscribe;
-
-public interface CreatNewCar {
-}
